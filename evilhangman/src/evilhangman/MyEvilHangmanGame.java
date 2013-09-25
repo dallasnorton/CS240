5f1d9fdeb788b35adb95e70eb240f33f75a3c331
Dallas Norton <dallaslandonnorton@gmail.com>
2013-09-25 15:41:23 -0600

    MOAR

---
+++ evilhangman/src/evilhangman/MyEvilHangmanGame.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evilhangman;
+
+import java.io.File;
+import java.util.Set;
+
+/**
+ *
+ * @author dallasnorton
+ */
+public class MyEvilHangmanGame implements EvilHangmanGame{
+
+    @Override
+    public void startGame(File dictionary, int wordLength) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Set<String> makeGuess(char guess) throws GuessAlreadyMadeException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
