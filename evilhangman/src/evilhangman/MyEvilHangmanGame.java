3bda8b811d9d58de9056583b3005f555d69e59b1
Dallas Norton <dallaslandonnorton@gmail.com>
2013-09-29 22:03:00 -0600

    lotsa code and no testing

--- evilhangman/src/evilhangman/MyEvilHangmanGame.java
+++ evilhangman/src/evilhangman/MyEvilHangmanGame.java
@@ -61,23 +61,108 @@ public class MyEvilHangmanGame implements EvilHangmanGame{
             wordMap.put(key, bucket);
         }
         
-        setNewDictionary();
+        bucket = setNewDictionary(guess);
         
         return bucket;
     }
     
-    public void setNewDictionary(){
-        Set<String> newDictionary = new HashSet<String>();
-        for (Map.Entry<String, Set<String>> entry : wordMap.entrySet())
-        {
-            newDictionary = entry.getValue();
-            if(newDictionary.size() > dictionary.size()){
-                dictionary = newDictionary;
-            }
-//            System.out.println(entry.getKey() + "/" + entry.getValue());
+    public Set<String> setNewDictionary(char guess){
+        Set tempSet = wordMap.entrySet();
+        Map.Entry<String, Set<String>> largestEntry = null;                
+        for(Iterator i = tempSet.iterator();i.hasNext();){
+            largestEntry = (Map.Entry)i.next();
         }
+                
+        for(Map.Entry<String, Set<String>> entry : wordMap.entrySet()){
+            if(entry.getValue().size() > largestEntry.getValue().size()){
+                largestEntry = entry;
+                continue;
+            }
+            if(entry.getValue().size() == largestEntry.getValue().size()){
+                String largeStr = largestEntry.getKey();
+                String entryStr = entry.getKey();
+                
+                if(largeStr.contains(Character.toString(guess)) && !entryStr.contains(Character.toString(guess))){
+                    largestEntry = entry;
+                    continue;
+                }
+                else{
+                    if(largeStr.contains(Character.toString(guess)) && entryStr.contains(Character.toString(guess))){
+                        //next check
+                        int largeStrCount = getCountForNumbersOfTimesCharsInWord(largeStr, guess);
+                        int entryStrCount = getCountForNumbersOfTimesCharsInWord(entryStr, guess);
+                        if(largeStrCount > entryStrCount){
+                            largestEntry = entry;
+                        }
+                        else{
+                            if(largeStrCount == entryStrCount){
+                                //next check - get rightmost guess letter
+                                int largeRightIndex = getRightMostIndex(largeStr, guess);
+                                int entryRightIndex = getRightMostIndex(entryStr, guess);
+                                if(entryRightIndex > largeRightIndex){
+                                    largestEntry = entry;
+                                    continue;
+                                }
+                                else{
+                                    if(entryRightIndex == largeRightIndex){
+                                        //next check - get next rightmost guess letter
+                                        int nextLargeRightIndex = getNextRightMostIndex(largeStr, guess, largeRightIndex);
+                                        int nextEntryRightIndex = getNextRightMostIndex(entryStr, guess, entryRightIndex);
+                                        if(nextEntryRightIndex > nextLargeRightIndex){
+                                            largestEntry = entry;
+                                            continue;
+                                        }
+                                        else if(nextLargeRightIndex > nextEntryRightIndex){
+                                            continue;
+                                        }
+                                        else{
+                                            largeRightIndex = nextLargeRightIndex;
+                                            entryRightIndex = nextEntryRightIndex;
+                                            nextLargeRightIndex = getNextRightMostIndex(largeStr, guess, largeRightIndex);
+                                            nextEntryRightIndex = getNextRightMostIndex(entryStr, guess, entryRightIndex);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return largestEntry.getValue();
+    }
     
-//        dictionary = newDictionary;
+    public int getNextRightMostIndex(String str, char guess, int index){
+        char[] ary = str.toCharArray();
+        int rightIndex = -1;
+        for(int i = ary.length; i > 0; i--){
+            if(ary[i] == guess && rightIndex > -1 && i != index){
+                rightIndex = i;
+                break;
+            }
+        }
+        return rightIndex;
+    }
+    public int getRightMostIndex(String str, char guess){
+        char[] ary = str.toCharArray();
+        int rightIndex = -1;
+        for(int i = ary.length; i > 0; i--){
+            if(ary[i] == guess && rightIndex > -1){
+                rightIndex = i;
+                break;
+            }
+        }
+        return rightIndex;
+    }
+    
+    public int getCountForNumbersOfTimesCharsInWord(String str, char guess){
+        int count = 0;
+        for(char c : str.toCharArray()){
+            if(c == guess){
+                count++;
+            } 
+        } 
+        return count;
     }
     
     public String convertToDashedString(String str, char guess){
